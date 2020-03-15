a279ae66cb3ed15b50c51261cbc74da019cb777a
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-03-15 16:40:57 -0600

    Initial Commit

---
+++ practice-git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practice-git
+//
+//  Created by Holt Bolt on 3/15/20.
+//  Copyright © 2020 Amy Holt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
